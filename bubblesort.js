c22bd1e7ad1ccd58b231e679a3fc2913028c9ae4
Victor Cheng <victorcheng23@gmail.com>
2019-09-04 13:07:30 -0400

    bubblesort kind of working

---
+++ bubblesort.js
@@ -0,0 +1,29 @@
+//[2, 1, 4, 3]
+
+//for 8 numbers, go through
+
+function bubbleSort(array) {
+  let arrLength = 0;
+  let swapCount = 0;
+  while (swapCount < array.length * 2 - 1) {
+    console.log(`#${arrLength}, ${array}`);
+    arrLength++;
+    for (let i = 0; i < array.length - 1; i++) {
+      if (array[i] > array[i + 1]) {
+        array = swap(i, array);
+        swapCount++;
+        console.log("swapCount: ", swapCount);
+      }
+    }
+  }
+
+  return array;
+}
+
+function swap(index, array) {
+  const firstNumber = array[index];
+  const secondNumber = array[index + 1];
+  array.splice(index, 1, secondNumber);
+  array.splice(index + 1, 1, firstNumber);
+  return array;
+}
---
+++ bubblesort.spec.js
@@ -0,0 +1,28 @@
+describe("Bubble Sort", function() {
+  // beforeAll(function() {
+  //   spyOn(this, "swap").and.callThrough();
+  // });
+  it("handles an empty array", function() {
+    expect(bubbleSort([])).toEqual([]);
+  });
+  it("handles a single item", function() {
+    expect(bubbleSort([1])).toEqual([1]);
+  });
+  it("handles multiple items", function() {
+    expect(bubbleSort([8, 3, 2, 6, 4, 9, 1, 7, 5])).toEqual([
+      1,
+      2,
+      3,
+      4,
+      5,
+      6,
+      7,
+      8,
+      9
+    ]);
+  });
+  it("calls on the swap function ", function() {
+    bubbleSort([2, 1, 4, 3]);
+    expect(swap.calls.count()).toEqual(2);
+  });
+});
